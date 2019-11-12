ef2c03432fc3f3a8418ce8d97c7bcc71e7b2a186
colinkanobe <30920587+colinkanobe@users.noreply.github.com>
2019-11-11 21:37:59 -0500

    commit changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
 import CoursesPage from "./components/courses/CoursesPage";
-import ManageCoursePage from "./components/courses/ManageCoursePage";
+import ManageCoursePage from "./components/courses/ManageCoursePage"; // eslint-disable-line import/no-named-as-default
 import Header from "./components/common/Header";
 import PageNotFound from "./components/PageNotFound";
 import { ToastContainer } from "react-toastify";
