56b23a117d8ccc3d343cc0792fa0205c11acadcf
colinkanobe <30920587+colinkanobe@users.noreply.github.com>
2020-04-06 12:46:22 -0400

    port 8080

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -43,7 +43,7 @@ server.post("/courses/", function(req, res, next) {
 server.use(router);
 
 // Start server
-const port = 7878;
+const port = 8080;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
